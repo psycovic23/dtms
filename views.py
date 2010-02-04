1268e6d7a428a41a4b66eecfac17df3a7475ee8d
psycovic23 <psycovic@gmail.com>
2010-02-04 17:31:38 -0500

    file cleanup. from now on this will be ONLY production machine worthy

--- views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-import datetime
-
-def current_datetime(request, accept):
-	#now = datetime.datetime.now()
-	html = "<html><body>%s %s</body></html>" % (now,accept)
-	return HttpResponse(html)
