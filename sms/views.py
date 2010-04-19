06d42ba8836a96071940851030acda72bd54c3c6
psycovic23 <psycovic@gmail.com>
2010-04-19 14:42:16 -0400

    namespace changes

--- sms/views.py
+++ sms/views.py
@@ -1,6 +1,11 @@
 # Create your views here.
 from django.http import HttpResponse
 
+from dtms.models import User
+
 def process_sms(request, sms_string):
+    words = sms_string.split('+')
+
+    
     return HttpResponse(sms_string)
 
